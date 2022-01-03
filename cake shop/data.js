f5451f45973915d4f1112f39bb7acf9d9ee65e87
1tangyen <51384739+1tangyen@users.noreply.github.com>
2022-01-03 11:27:46 -0500

    Add files via upload

---
+++ cake shop/data.js
@@ -0,0 +1,76 @@
+const cake = [
+	{
+		id: 1,
+		title: 'cupcake-1',
+		price: 10.99,
+		image: './images/cupcake-1.jpeg'
+	},
+	{
+		id: 2,
+		title: 'cupcake-2',
+		price: 10.99,
+		image: './images/cupcake-2.jpeg'
+	},
+	{
+		id: 3,
+		title: 'cupcake-3',
+		price: 10.99,
+		image: './images/cupcake-3.jpeg'
+	},
+	{
+		id: 4,
+		title: 'cupcake-4',
+		price: 10.99,
+		image: './images/cupcake-4.jpeg'
+	},
+	{
+		id: 5,
+		title: 'cake-1',
+		price: 10.99,
+		image: './images/cake-1.jpeg'
+	},
+	{
+		id: 6,
+		title: 'cake-2',
+		price: 10.99,
+		image: './images/cake-2.jpeg'
+	},
+	{
+		id: 7,
+		title: 'cake-3',
+		price: 10.99,
+		image: './images/cake-3.jpeg'
+	},
+	{
+		id: 8,
+		title: 'cake-4',
+		price: 10.99,
+		image: './images/cake-4.jpeg'
+	},
+	{
+		id: 9,
+		title: 'cake-5',
+		price: 10.99,
+		image: './images/cake-5.jpeg'
+	},
+	{
+		id: 10,
+		title: 'cake-6',
+		price: 10.99,
+		image: './images/cake-6.jpeg'
+	},
+	{
+		id: 11,
+		title: 'weddingcake-1',
+		price: 10.99,
+		image: './images/cake-1.jpeg'
+	},
+	{
+		id: 12,
+		title: 'weddingcake-2',
+		price: 10.99,
+		image: './images/cake-2.jpeg'
+	}
+];
+
+export default cake;
